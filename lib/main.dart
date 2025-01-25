e038e998ae36ea210eb917e04fd051a3180e643e
Aswinazikar <azwinaswin8@gmail.com>
2025-01-25 09:38:23 +0530

    feat: integrate Sizer and add easy_sidemenu dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:salesorg_adminapp/view/home_page/home_page.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,12 +9,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const HomePage(),
+    return Sizer(
+      builder: (context, orientation, deviceType) => const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      ),
     );
   }
 }
