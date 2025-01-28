1df4a06c1129f8ea21d6e7edb16e5ee671946c49
cbabi2023 <abijithcbbsccs2021@gmail.com>
2025-01-28 22:02:17 +0530

    feat: enhance MyApp structure with comments for clarity and responsiveness

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,27 @@
-import 'package:flutter/material.dart';
-import 'package:salesorg_adminapp/routes/routes.dart';
 
-import 'package:sizer/sizer.dart';
+import 'package:flutter/material.dart'; 
+import 'package:salesorg_adminapp/routes/routes.dart'; // Custom routes for navigation
+import 'package:sizer/sizer.dart'; // Provides responsive sizing for different screen sizes
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp()); // Runs the MyApp widget
 }
 
+// MyApp is a StatelessWidget that defines the overall structure of the app
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key}); // Constructor for MyApp widget
 
   @override
   Widget build(BuildContext context) {
+    // Sizer widget is used here for responsive UI, adjusting size based on screen size and orientation
     return Sizer(
       builder: (context, orientation, deviceType) => const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        initialRoute: Approutes.navScreen,
-        onGenerateRoute: Approutes.generateRoute,
+        debugShowCheckedModeBanner: false, // Disables the debug banner in the app
+        initialRoute: Approutes.navScreen, // Specifies the initial route to navigate to
+        onGenerateRoute: Approutes.generateRoute, // Defines how the routes are generated in the app
       ),
     );
   }
 }
+
